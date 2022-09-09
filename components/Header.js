5798ef31451a7970f328e90192b9e2040ee95099
Rohnit Shriyan <99482544+RohnitShriyan05@users.noreply.github.com>
2022-09-09 23:41:55 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { CgProfile } from "react-icons/cg";
 export default function Header() {
+    const router = useRouter();
   return (
     <div className="w-full px-2vw pt-1vh border-b flex items-center">
       <Link href="/">
         <p className="flex-1 vsmalltext"><span className="logo bigtext pr-2 cursor-pointer">Instagram</span>by Rohnit Shriyan</p>
       </Link>
       <Link href="/Profile">
-        <CgProfile className="bigtext cursor-pointer" />
+        <CgProfile className={`bigtext cursor-pointer ${router.asPath==="/Profile" ? "hidden": "block"}`} />
       </Link>
     </div>
   );
